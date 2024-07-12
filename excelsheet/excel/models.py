67c532154418fe919715e3b8d2ad8963f979519a
vijaykumarr1452 <115397037+vijaykumarr1452@users.noreply.github.com>
2024-07-12 18:54:08 +0530

    Add files via upload

---
+++ excelsheet/excel/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Vijay(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    org = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+     
+    
+
